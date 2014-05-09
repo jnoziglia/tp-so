e3b510173408b0cdc1aa474d43689cf011cc5b7d
matiasfarran <matiasfarran66@gmail.com>
2014-05-09 16:40:13 -0300

    Inicio y primeras líneas del Kernel

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -10,8 +10,52 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+
+/* Estructuras de datos */
+typedef struct pcb
+{
+	int pid;
+	int segmentoCodigo;
+	int segmentoStack;
+	int cursorStack;
+	int indiceCodigo;
+	int indiceEtiquetas;
+	int programCounter;
+	int tamanioContextoActual;
+	int tamanioIndiceEtiquetas;
+};
+
+/* Funciones */
+void* hiloPCP();
+void* hiloPLP();
+
+/* Variables Globales */
+void* new = NULL;
+void* ready = NULL;
+void* exec = NULL;
+void* exit = NULL;
+
 
 int main(void) {
-	puts("holis"); /* prints holis */
+	pthread_t hiloPCP, hiloPLP;
+	int rhPCP, rhPLP;
+	rhPCP = pthread_create(&hiloPCP, NULL, (void*)hiloPCP, NULL);
+	rhPLP = pthread_create(&hiloPLP, NULL, (void*)hiloPLP, NULL);
+	pthread_join(hiloPCP, NULL);
+	pthread_join(hiloPLP, NULL);
+	printf("%d",rhPCP);
+	printf("%d",rhPLP);
 	return EXIT_SUCCESS;
+}
+
+
+void* hiloPCP()
+{
+	return 0;
+}
+
+void* hiloPLP()
+{
+	pthread_t esperarConexionesPrograma,
 }
